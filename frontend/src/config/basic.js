0f84e51926c64fb49b042bd99c3a5578f456ce3b
root <noreply@wutnews.net>
2020-09-15 11:52:29 +0000

    [fix] change domain

--- frontend/src/config/basic.js
+++ frontend/src/config/basic.js
@@ -1,5 +1,5 @@
-let frontDomain = process.env.TOKEN_FRONT_DOMAIN ? process.env.TOKNE_FRONT_DOMAIN : "http://token.vaiwan.com"
-let backendDomain = process.env.TOKEN_BACKEND_DOMAIN ? process.env.TOKEN_BACKEND_DOMAIN : "http://localhost:8000"
+let frontDomain = process.env.TOKEN_FRONT_DOMAIN ? process.env.TOKNE_FRONT_DOMAIN : "https://join.itoken.team"
+let backendDomain = process.env.TOKEN_BACKEND_DOMAIN ? process.env.TOKEN_BACKEND_DOMAIN : "https://join.itoken.team/api"
 
 const Config = {
 	//前端的域名（必须是域名，不能是ip:port形式），注意域名最后不要加斜杠
